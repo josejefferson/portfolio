18437ef56a27ed51d238e0252c585233ad009a7c
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-05-27 14:50:10 +0000

    feat: Setup Next.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true
+}
 
 module.exports = nextConfig
