7ca6bfffa01e77b487e442d1bb55ba4ebd23851b
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2024-05-04 13:07:42 -0300

    fix: Corrigido links na seção experiência

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -2,6 +2,7 @@ import { Container, Heading, UnorderedList, ListItem, Text, Box, Flex, Tooltip }
 import { SECTION_IDS } from '.'
 import Section from '../common/section'
 import { COMPETENCES } from '#/data/competences'
+import Link from '../common/link'
 
 export default function Experience() {
   return (
@@ -15,18 +16,35 @@ export default function Experience() {
 
         <UnorderedList>
           <ListItem>
-            Desenvolvi uma plataforma para registro de faltas dos professores e remanejamento de horários escolares;
+            Desenvolvi uma plataforma para registro de faltas dos professores e remanejamento de horários escolares{' '}
+            <Link href="/projeto/vagazero/" target="_blank" color="primary.400">
+              (link)
+            </Link>
+            ;
           </ListItem>
           <ListItem>
             Criei um assistente virtual (chatbot) para auxiliar na demanda de dúvidas de alunos e responsáveis de alunos
-            do IFPB (link). Este projeto recebeu uma premiação na VII FECAP;
+            do IFPB{' '}
+            <Link href="/projeto/chatbot/" target="_blank" color="primary.400">
+              (link)
+            </Link>
+            .<br />
+            Este projeto recebeu uma premiação na VII FECAP;
           </ListItem>
           <ListItem>
             Criei uma extensão para o Google Meet que permite a leitura em voz alta das mensagens do chat, para ajudar
-            os professores durante a pandemia (link);
+            os professores durante a pandemia{' '}
+            <Link href="https://github.com/josejefferson/speak-meet-messages" target="_blank" color="primary.400">
+              (link)
+            </Link>
+            ;
           </ListItem>
           <ListItem>
-            Desenvolvi um site que reunia todos as videoaulas gravadas pelo IFPB durante a pandemia (link).
+            Desenvolvi um site que reunia todos as videoaulas gravadas pelo IFPB durante a pandemia{' '}
+            <Link href="https://github.com/josejefferson/ifpblives" target="_blank" color="primary.400">
+              (link)
+            </Link>
+            .
           </ListItem>
         </UnorderedList>
 
